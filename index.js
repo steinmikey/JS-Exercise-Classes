9165ae7019e9960943f3332832eb3f10f30ff5b7
Michael Steiner <michaelvsteiner@gmail.com>
2021-07-22 15:18:51 -0600

    Solve Task 4

--- index.js
+++ index.js
@@ -132,7 +132,20 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {}
+class Instructor extends Lambdasian {
+  constructor(attrs1) {
+    super(attrs1);
+    this.specialty = attrs1.specialty;
+    this.favLanguage = attrs1.favLanguage;
+    this.catchPhrase = attrs1.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
